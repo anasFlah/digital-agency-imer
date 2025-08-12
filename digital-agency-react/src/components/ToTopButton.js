7fca66d2bb2b882fe99e8a64185212975b69651d
Anas_Flah <aneshp0258@gmail.com>
2025-08-12 12:53:09 +0100

    fixing front

---
+++ digital-agency-react/src/components/ToTopButton.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+const ToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show button when page is scrolled up to given distance
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Set the top cordinate to 0
+  // make scrolling smooth
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <>
+      {isVisible && (
+        <button
+          className="scroll-to-top visible"
+          onClick={scrollToTop}
+          aria-label="Scroll to top"
+        >
+          <i className="ph ph-arrow-up"></i>
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ToTopButton;
